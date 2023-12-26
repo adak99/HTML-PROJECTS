5246f827e23ebff4380e95564830d9831886063e
adak99 <soumyadipadak99@gmail.com>
2023-12-26 19:50:07 +0530

    Update

---
+++ back_end_java_code.java
@@ -0,0 +1,11 @@
+public class back_end_java_code {
+    public static void main(String[] args) {
+
+        int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0;
+
+        if (a == 1 || b == 2 || c == 3 || d == 4 || e == 1 || f == 2 || g == 4 || h == 3)
+            System.out.println("Correct.");
+        else
+            System.out.println("Wrong");
+    }
+}
